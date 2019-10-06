bbcab5fd57e20eed0bfa3e892e92f66dd3c1114a
kudo <yosuke.kudo910@gmail.com>
2019-10-06 23:01:57 +0900

    Test normalize Either

--- src/modules/darts/schema.ts
+++ src/modules/darts/schema.ts
@@ -1,5 +1,6 @@
 import { schema, NormalizedSchema, normalize } from 'normalizr';
 import { Dart, FetchDartsResponse } from 'modules/darts/types';
+import { Either, tryCatch } from 'fp-ts/lib/Either';
 
 export const dartSchema = new schema.Entity('darts');
 export const dartListSchema = [dartSchema];
@@ -23,3 +24,14 @@ export const dartNormalize = (data: Dart): NormalizedEntity<Dart> =>
 export const dartsNormalize = (
   data: FetchDartsResponse,
 ): NormalizedEntities<Dart> => normalize(data.darts, dartListSchema);
+
+// export const dartsNormalize_ = (data: FetchDartsResponse) =>
+//   tryCatch(
+//     () => dartsNormalize(data),
+//     e => {
+//       console.log(e);
+//       // throw new NormalizeError(e);
+//     },
+//   );
+
+// export const dartsNormalize = (data: Dart) => dartsNormalizeEither(data);
