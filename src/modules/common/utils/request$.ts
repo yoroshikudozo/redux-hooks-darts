f7b6a20b83eb630deac3b32385ae542799f9ab7f
kudo <yosuke.kudo910@gmail.com>
2019-10-22 23:08:39 +0900

    Refactoring request$

--- src/modules/common/utils/request$.ts
+++ src/modules/common/utils/request$.ts
@@ -1,5 +1,6 @@
 import { Observable, of } from 'rxjs';
 import { switchMap, catchError } from 'rxjs/operators';
+import { fromFetch } from 'rxjs/fetch';
 
 export const wrap$ = <T, E>(task: Observable<Response>): Observable<T | E> => {
   return task.pipe(
@@ -33,58 +34,9 @@ export interface TypedResponse<T = any> extends Response {
   json<P = T>(): Promise<P>;
 }
 
-declare function fetch<T>(...args: any): Promise<TypedResponse<T>>;
-
-export function fromFetch<T>(
-  input: string | Request,
+const http$ = <T>(
+  input: RequestInfo,
   init?: RequestInit,
-): Observable<TypedResponse<T>> {
-  return new Observable<Response>(subscriber => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    let outerSignalHandler: () => void;
-    let abortable = true;
-    let unsubscribed = false;
-
-    if (init) {
-      // If a signal is provided, just have it teardown. It's a cancellation token, basically.
-      if (init.signal) {
-        outerSignalHandler = () => {
-          if (!signal.aborted) {
-            controller.abort();
-          }
-        };
-        init.signal.addEventListener('abort', outerSignalHandler);
-      }
-      init.signal = signal;
-    } else {
-      init = { signal };
-    }
-
-    fetch<T>(input, init)
-      .then(response => {
-        abortable = false;
-        subscriber.next(response);
-        subscriber.complete();
-      })
-      .catch(err => {
-        abortable = false;
-        if (!unsubscribed) {
-          // Only forward the error if it wasn't an abort.
-          subscriber.error(err);
-        }
-      });
-
-    return () => {
-      unsubscribed = true;
-      if (abortable) {
-        controller.abort();
-      }
-    };
-  });
-}
-
-const http$ = <T = any, E = any>(input: RequestInfo, init?: RequestInit) =>
-  fromFetch<T>(input, init);
+): Observable<TypedResponse<T>> => fromFetch(input, init);
 
 export default http$;
